a69886ed4607794d51968eacfa5e7ac7bda9a19d
Maria Mihai <maria.ic.mihai@gmail.com>
2020-04-18 12:04:18 +0200

    Initial commit. #1

---
+++ src/test/java/guru/springframework/monolith/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
