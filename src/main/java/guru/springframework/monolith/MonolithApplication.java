9cdda523e1dc0d35a2a6c22f9dc7fd586055d44a
Maria Mihai <maria.ic.mihai@gmail.com>
2020-04-18 13:15:24 +0200

    Removed args to fix security hotspot. #4

--- src/main/java/guru/springframework/monolith/MonolithApplication.java
+++ src/main/java/guru/springframework/monolith/MonolithApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonolithApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MonolithApplication.class, args);
+        SpringApplication.run(MonolithApplication.class);
     }
 
 }
