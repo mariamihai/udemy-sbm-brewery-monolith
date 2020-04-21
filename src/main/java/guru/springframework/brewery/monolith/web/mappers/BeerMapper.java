bf86ce854f4a45aeb4503ad15a196a6804718a1a
Maria Mihai <maria.ic.mihai@gmail.com>
2020-04-21 19:31:39 +0200

    Add mappers for beer and date #17

---
+++ src/main/java/guru/springframework/brewery/monolith/web/mappers/BeerMapper.java
@@ -0,0 +1,14 @@
+package guru.springframework.brewery.monolith.web.mappers;
+
+import guru.springframework.brewery.monolith.domain.Beer;
+import guru.springframework.brewery.monolith.web.model.BeerDto;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(uses = {DateMapper.class}, componentModel  = "spring")
+@Component
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
